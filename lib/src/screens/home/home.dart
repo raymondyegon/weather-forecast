a7aaa1cb1d5b280c084dbaf20c6fb03d668d3b98
raymondyegon <raymondyegon17@gmail.com>
2021-04-15 01:10:29 +0300

    Add routing page to specific city page

--- lib/src/screens/home/home.dart
+++ lib/src/screens/home/home.dart
@@ -72,116 +72,122 @@ class _HomeState extends State<Home> {
                   itemCount: 4,
                   physics: ClampingScrollPhysics(),
                   itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: Colors.greenAccent,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 2.0.w,
-                        vertical: 0.75.h,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ListTile(
-                            // tileColor: Colors.amber,
-                            contentPadding: EdgeInsets.zero,
-                            title: Text(
-                              '20\u00B0',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 25.0.sp,
-                              ),
-                            ),
-                            trailing: CachedNetworkImage(
-                              height: 60,
-                              width: 60,
-                              placeholder: (context, url) => Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                              imageUrl:
-                                  'http://openweathermap.org/img/wn/10d@2x.png',
-                              imageBuilder: (context, imageProvider) =>
-                                  Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    image: imageProvider,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              errorWidget: (_, url, error) {
-                                return Icon(Icons.error);
-                              },
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Text(
-                                'Bangkok',
+                    return InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/city');
+                      },
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 2.0.w,
+                          vertical: 0.75.h,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListTile(
+                              // tileColor: Colors.amber,
+                              contentPadding: EdgeInsets.zero,
+                              title: Text(
+                                '20\u00B0',
                                 style: TextStyle(
                                   color: Colors.black,
-                                  fontSize: 12.0.sp,
+                                  fontSize: 25.0.sp,
                                 ),
                               ),
-                              Text(
-                                'UK',
-                                style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 11.0.sp,
+                              trailing: CachedNetworkImage(
+                                height: 60,
+                                width: 60,
+                                placeholder: (context, url) => Center(
+                                  child: CircularProgressIndicator(),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Icon(
-                                    FlutterIcons.water_mco,
-                                    color: Colors.blue,
-                                    size: 17.0.sp,
-                                  ),
-                                  SizedBox(width: 1.0.w),
-                                  Text(
-                                    '27%',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 11.0.sp,
+                                imageUrl:
+                                    'http://openweathermap.org/img/wn/10d@2x.png',
+                                imageBuilder: (context, imageProvider) =>
+                                    Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
                                     ),
                                   ),
-                                ],
+                                ),
+                                errorWidget: (_, url, error) {
+                                  return Icon(Icons.error);
+                                },
                               ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Icon(
-                                    FlutterIcons.wind_faw5s,
-                                    color: Colors.blue,
-                                    size: 15.0.sp,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Text(
+                                  'Bangkok',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12.0.sp,
                                   ),
-                                  SizedBox(width: 1.0.w),
-                                  Text(
-                                    '7 km/h',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 11.0.sp,
+                                ),
+                                Text(
+                                  'UK',
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 11.0.sp,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      FlutterIcons.water_mco,
+                                      color: Colors.blue,
+                                      size: 17.0.sp,
                                     ),
-                                  )
-                                ],
-                              )
-                            ],
-                          )
-                        ],
+                                    SizedBox(width: 1.0.w),
+                                    Text(
+                                      '27%',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11.0.sp,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      FlutterIcons.wind_faw5s,
+                                      color: Colors.blue,
+                                      size: 15.0.sp,
+                                    ),
+                                    SizedBox(width: 1.0.w),
+                                    Text(
+                                      '7 km/h',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11.0.sp,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
