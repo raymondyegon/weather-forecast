c1e0ef1c5a34b5d2868da1958aef396c3f341064
raymondyegon <raymondyegon17@gmail.com>
2021-04-15 19:29:30 +0300

    Created function to get current weather when app laucnhes

--- lib/src/appstate_container.dart
+++ lib/src/appstate_container.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:weather/src/model/city_model.dart';
+import 'package:weather/src/model/forecast_model.dart';
+import 'package:weather/src/network/api_service.dart';
 import 'package:weather/src/service_locator.dart';
 
 class _InheritedStateContainer extends InheritedWidget {
@@ -79,9 +81,57 @@ class StateContainerState extends State<StateContainer> {
     ),
   ];
 
+  // To check help in showing a loader while fetching data from api
+  bool isLoading = false;
+
+  // conatins thecurrent day forecast
+  List<ForecastModel> currentWeather = [];
+
+  // Contains the 5 Day weather forecast
+  Map<int, List<ForecastModel>> weatherforecast = {};
+
   @override
   void initState() {
     super.initState();
+
+    // Load current weather for our cities
+    loadCityWeathers();
+  }
+
+  /// Load the weather of our given cities
+  void loadCityWeathers() async {
+    /// Show that loading to true
+    setState(() {
+      isLoading = true;
+    });
+
+    /// Loop through each of th 4 cities
+    for (var city in cities) {
+      // If we get atleast one result we set the loader to false
+      if (currentWeather.length > 0) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+
+      /// Get the current weather details
+      await getCurrentWeather(city.id);
+    }
+
+    /// Log the total number of current wether details tht we have... we asset it is 4
+    log.d(currentWeather.length);
+  }
+
+  /// Helper function to get the current weather of a city by its ID
+  Future<void> getCurrentWeather(int cityId) async {
+    ForecastModel weatherResult =
+        await sl.get<ApiService>().getCurrentWeather(cityID: cityId);
+
+    setState(
+      () {
+        currentWeather.add(weatherResult);
+      },
+    );
   }
 
   @override
