4dc8a882bcbaad18314658bf2dd7bdb68bed2642
raymondyegon <raymondyegon17@gmail.com>
2021-04-15 20:25:06 +0300

    Removed unncessary if else

--- lib/src/model/forecast_model.dart
+++ lib/src/model/forecast_model.dart
@@ -12,34 +12,19 @@ class ForecastModel {
   final int unixTime; // timesince epoch when it is forecasted
 
   factory ForecastModel.fromJson(Map<String, dynamic> json) {
-    /// If it contains the cnt keys then it is a 5 day forecast els its the current weather
-    if (json.containsKey('cnt')) {
-      var data = json['list'][0];
-      return ForecastModel(
-        cityID: json['city']['id'],
-        weather: WeatherModel.fromJson(data['weather'][0]),
-        windSpeed: data['wind']['speed'].toString(),
-        feelsLike: data['main']['feels_like'].toString(),
-        humidity: data['main']['humidity'].toString(),
-        temp: data['main']['temp'].toString(),
-        tempMax: data['main']['temp_max'].toString(),
-        tempMin: data['main']['temp_min'].toString(),
-        unixTime: data['dt'],
-      );
-    } else {
-      return ForecastModel(
-        weather: WeatherModel.fromJson(json['weather'][0]),
-        cityID: json['id'],
-        windSpeed: json['wind']['speed'].toString(),
-        feelsLike: json['main']['feels_like'].toString(),
-        temp: json['main']['temp'].toString(),
-        tempMin: json['main']['temp_min'].toString(),
-        tempMax: json['main']['temp_max'].toString(),
-        humidity: json['main']['humidity'].toString(),
-        unixTime: json['dt'],
-      );
-    }
+    return ForecastModel(
+      weather: WeatherModel.fromJson(json['weather'][0]),
+      cityID: json['id'] ?? null,
+      windSpeed: json['wind']['speed'].toString(),
+      feelsLike: json['main']['feels_like'].toString(),
+      temp: json['main']['temp'].toString(),
+      tempMin: json['main']['temp_min'].toString(),
+      tempMax: json['main']['temp_max'].toString(),
+      humidity: json['main']['humidity'].toString(),
+      unixTime: json['dt'],
+    );
   }
+
   ForecastModel({
     this.cityID,
     this.feelsLike,
