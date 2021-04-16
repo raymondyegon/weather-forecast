8da6c14fa40089d605f59db5d8281fec4ef38a8f
raymondyegon <raymondyegon17@gmail.com>
2021-04-16 12:09:09 +0300

    Added Firebase analytics & crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -6,11 +10,22 @@ import 'package:weather/src/app.dart';
 import 'package:weather/src/appstate_container.dart';
 import 'package:weather/src/service_locator.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Setup services
   setupServiceLocator();
+
+  // initialize firebase
+  await Firebase.initializeApp();
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  Function originalOnError = FlutterError.onError;
+  FlutterError.onError = (FlutterErrorDetails errorDetails) async {
+    await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    // Forward to original handler.
+    originalOnError(errorDetails);
+  };
 
   // Setup logger, only show warning and higher in release mode.
   if (kReleaseMode) {
@@ -22,10 +37,15 @@ void main() {
   // Set the orientation to potrait and start the app
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
     (_) {
-      runApp(
-        new StateContainer(
-          child: new App(),
-        ),
+      runZonedGuarded(
+        () {
+          runApp(
+            new StateContainer(
+              child: new App(),
+            ),
+          );
+        },
+        FirebaseCrashlytics.instance.recordError,
       );
     },
   );
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weather/src/screens/city/city.dart';
@@ -9,6 +11,10 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -22,6 +28,7 @@ class _AppState extends State<App> {
               debugShowCheckedModeBanner: false,
               title: 'Weather Tracker',
               initialRoute: '/',
+              navigatorObservers: <NavigatorObserver>[observer],
               onGenerateRoute: (RouteSettings settings) {
                 switch (settings.name) {
                   case '/':
